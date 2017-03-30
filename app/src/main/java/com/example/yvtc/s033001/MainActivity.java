20f69e658baaae0d7640e7f19c813ef5b9fb1d7d
dannielccw <dannielccw@gmail.com>
2017-03-30 10:44:52 +0800

    + OnItemClickListener

--- app/src/main/java/com/example/yvtc/s033001/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s033001/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.yvtc.s033001;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ListView Iv;
+    ListView lv;
     String fruits[] = {"apple", "orange", "banana"};
 
     @Override
@@ -14,9 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Iv = (ListView) findViewById(R.id.listView);
+        lv = (ListView) findViewById(R.id.listView);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, fruits);
-        Iv.setAdapter(adapter);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, fruits[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
